aaa67a6f6b19609faa78c6da05c6b1a16cc32e34
Andrea Mazzini <andrea.mazzini@gmail.com>
2014-11-10 10:58:38 +0100

    Added accessibility label for KIF UI testing

--- AMPopTip/AMPopTip.m
+++ AMPopTip/AMPopTip.m
@@ -335,6 +335,7 @@
 {
     self.attributedText = nil;
     self.text = text;
+    self.accessibilityLabel = text;
     self.direction = direction;
     self.containerView = view;
     self.maxWidth = maxWidth;
@@ -347,6 +348,7 @@
 {
     self.text = nil;
     self.attributedText = text;
+    self.accessibilityLabel = [text string];
     self.direction = direction;
     self.containerView = view;
     self.maxWidth = maxWidth;
