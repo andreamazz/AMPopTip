7c092650b8ffdefb1d678cc26fcb52f183ffada3
Andrea Mazzini <andrea.mazzini@gmail.com>
2015-06-13 16:56:24 +0200

    Fix carthage

--- AMPopTip/AMPopTip.h
+++ AMPopTip/AMPopTip.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char AMPopTipVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AMPopTip/PublicHeader.h>
 
-
+#import <AMPopTip/AMPopTip.h>
