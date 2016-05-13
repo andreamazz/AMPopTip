d46b01f30e668c0735530e8e42c6e216b515e988
Andrea Mazzini <andrea.mazzini@gmail.com>
2016-05-13 12:18:01 +0200

    Embed pods for faster CI times

---
+++ PopTipDemo/Pods/Target Support Files/AMPopTip/AMPopTip-umbrella.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+#import "AMPopTip+Animation.h"
+#import "AMPopTip+Draw.h"
+#import "AMPopTip+Entrance.h"
+#import "AMPopTip+Exit.h"
+#import "AMPopTip.h"
+#import "AMPopTipDefaults.h"
+
+FOUNDATION_EXPORT double AMPopTipVersionNumber;
+FOUNDATION_EXPORT const unsigned char AMPopTipVersionString[];
+
---
+++ PopTipDemo/Pods/Target Support Files/Pods-PopTipDemo/Pods-PopTipDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PopTipDemo : NSObject
+@end
+@implementation PodsDummy_Pods_PopTipDemo
+@end
