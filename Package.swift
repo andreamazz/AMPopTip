9937ad247af9130b37ed3f20461815b54466f4c6
Simonas Daniliauskas <daniliauskas.s@gmail.com>
2019-09-18 16:19:09 +0300

    fix code style

--- Package.swift
+++ Package.swift
@@ -3,15 +3,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "AMPopTip",
-    products: [
-        .library(
-            name: "AMPopTip",
-            targets: ["AMPopTip"])
-    ],
-    targets: [
-        .target(
-            name: "AMPopTip",
-            path: "Source")
-    ]
+  name: "AMPopTip",
+  products: [
+    .library(
+      name: "AMPopTip",
+      targets: ["AMPopTip"])
+  ],
+  targets: [
+    .target(
+      name: "AMPopTip",
+      path: "Source")
+  ]
 )
