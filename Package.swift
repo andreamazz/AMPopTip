7f46f13c4c8097db85fc69b16e198a7fdd33d080
Tomas Korfant <tomas.korfant@nordlicht-it.com>
2021-09-23 11:16:55 +0200

    Update swift package manager with platform

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "AMPopTip",
+  platforms: [.iOS(.v12), .macOS(.v10_14), .tvOS(.v12), .watchOS(.v5)],
   products: [
     .library(
       name: "AMPopTip",
